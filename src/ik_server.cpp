4d0fac63989084abd106a2fcf35c6c3909f83bc6
Yoan Mollard <yoan.mollard@inria.fr>
2016-04-27 17:08:28 +0200

    Included as a service

--- src/ik_server.cpp
+++ src/ik_server.cpp
@@ -31,24 +31,25 @@ OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <trac_ik/trac_ik.hpp>
 #include <ros/ros.h>
 #include <kdl/chainiksolverpos_nr_jl.hpp>
+#include <baxter_core_msgs/SolvePositionIK.h>
+#include <sensor_msgs/JointState.h>
 
 class BaxterTracIKServer {
 private:
     std::string _side;
-    ros::NodeHandle _nh;
     double _timeout;
     std::string _urdf_param;
     TRAC_IK::TRAC_IK *_tracik_solver;
     KDL::Chain _chain;
     KDL::JntArray *_nominal;
+    ros::ServiceServer _service;
     bool _ready;
 
 public:
-    BaxterTracIKServer(ros::NodeHandle& nh, std::string side, double timeout, std::string urdf_param) {
+    BaxterTracIKServer(std::string side, double timeout, std::string urdf_param) {
         double eps = 1e-5;
         this->_ready = false;
         this->_side = side;
-        this->_nh = nh;
         this->_urdf_param = urdf_param;
         this->_timeout = timeout;
         this->_tracik_solver = new TRAC_IK::TRAC_IK("base", side + "_gripper", urdf_param, timeout, eps);
@@ -86,27 +87,49 @@ public:
         delete this->_nominal;
     }
 
-    void test()
-        {
-          KDL::JntArray result;
-          KDL::Frame end_effector_pose(KDL::Rotation::Quaternion(-0.113, 0.992, -0.026, 0.046),
-                                       KDL::Vector(0.611, -0.230, 0.085));
+    bool perform_ik(baxter_core_msgs::SolvePositionIK::Request &request,
+                    baxter_core_msgs::SolvePositionIK::Response &response) {
 
           int rc;
-          rc = this->_tracik_solver->CartToJnt(*(this->_nominal), end_effector_pose, result);
+          KDL::JntArray result;
+          sensor_msgs::JointState joint_state;
+          for(uint segment=0; segment<this->_chain.getNrOfSegments(); ++segment) {
+              KDL::Joint joint = this->_chain.getSegment(segment).getJoint();
+              if(joint.getType()!=KDL::Joint::None)
+                  joint_state.name.push_back(joint.getName());
+          }
 
-          if (rc>=0)
-              ROS_INFO("SUCCESS");
-          else
-              ROS_ERROR("FAILURE");
+          for(uint point=0; point<request.pose_stamp.size(); ++point) {
+              KDL::Frame end_effector_pose(KDL::Rotation::Quaternion(request.pose_stamp[point].pose.orientation.x,
+                                                                     request.pose_stamp[point].pose.orientation.y,
+                                                                     request.pose_stamp[point].pose.orientation.z,
+                                                                     request.pose_stamp[point].pose.orientation.w),
+                                           KDL::Vector(request.pose_stamp[point].pose.position.x,
+                                                       request.pose_stamp[point].pose.position.y,
+                                                       request.pose_stamp[point].pose.position.z));
+
+              rc = this->_tracik_solver->CartToJnt(*(this->_nominal), end_effector_pose, result);
+
+              if(rc>=0) {
+                  for(uint joint=0; joint<this->_chain.getNrOfJoints(); ++joint) {
+                      joint_state.position.push_back(result(joint));
+                  }
+                  response.joints.push_back(joint_state);
+                  response.isValid.push_back(true);
+              }
+              else {
+                  response.isValid.push_back(false);
+              }
+          }
+          return true;
         }
 };
 
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "ik_server");
-  ros::NodeHandle nh("~");
+  ros::init(argc, argv, "trac_ik_baxter");
+  ros::NodeHandle nh;
 
   std::string urdf_param;
   double timeout;
@@ -114,8 +137,9 @@ int main(int argc, char** argv)
   nh.param("timeout", timeout, 0.005);
   nh.param("urdf_param", urdf_param, std::string("/robot_description"));
 
-  BaxterTracIKServer ik(nh, "right", timeout, urdf_param);
-  ik.test();
+  BaxterTracIKServer ik("right", timeout, urdf_param);
+  ros::ServiceServer service = nh.advertiseService("compute_ik", &BaxterTracIKServer::perform_ik, &ik);
+  ros::spin();
 
   return 0;
 }
